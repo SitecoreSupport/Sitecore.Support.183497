f01f597d366eab3ad4d20fc90bb15e51d98aa8c0
Kirill Volovitskiy <kiv@sitecore.net>
2017-11-20 11:21:16 +0200

    bug fixed

---
+++ src/Sitecore.Support.183497/Pipelines/HttpRequest/ItemResolver.cs
@@ -0,0 +1,34 @@
+﻿namespace Sitecore.Support.Pipelines.HttpRequest
+{
+  using Sitecore.Abstractions;
+  using Sitecore.Configuration;
+  using Sitecore.Data.ItemResolvers;
+  using Sitecore.Data.Items;
+  using Sitecore.Pipelines.HttpRequest;
+  using System;
+  public class ItemResolver : Sitecore.Pipelines.HttpRequest.ItemResolver
+  {
+    [Obsolete("Please use another constructor with parameters")]
+    public ItemResolver() : base()
+    {
+    }
+
+    public ItemResolver(BaseItemManager itemManager, ItemPathResolver pathResolver) : base(itemManager, pathResolver, Settings.ItemResolving.FindBestMatch)
+    {
+    }
+
+    protected override Item ResolveByMixedDisplayName(HttpRequestArgs args, out bool accessDenied)
+    {
+      Item resolvedItem = base.ResolveByMixedDisplayName(args, out accessDenied);
+      args.PermissionDenied = accessDenied;
+      return resolvedItem;
+    }
+
+    protected override bool TryResolveItem(string itemPath, HttpRequestArgs args, out Item item, out bool permissionDenied)
+    {
+      bool tryResolveItem = base.TryResolveItem(itemPath, args, out item, out permissionDenied);
+      args.PermissionDenied = permissionDenied;
+      return tryResolveItem;
+    }
+  }
+}
---
+++ src/Sitecore.Support.183497/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.183497")]
+[assembly: AssemblyProduct("Sitecore.Support.183497")]
+[assembly: ComVisible(false)]
